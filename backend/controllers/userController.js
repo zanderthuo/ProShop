b3edac7b8024ad32492e77c2aa2b900e8156b8ff
zanderthuo <thuozander@gmail.com>
2023-01-12 22:42:39 +0300

    Added shipping module

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,7 +77,7 @@ export const registerUser = async(req, res, next) => {
 
 
 // @desc  Get user profile
-// @route GET /api/users/login
+// @route GET /api/users/profile
 // @access Private
 export const getUserProfile = async(req, res, next) => {
   try {
@@ -90,6 +90,40 @@ export const getUserProfile = async(req, res, next) => {
         email: user.email,
         isAdmin: user.isAdmin,
       })
+    } else {
+      res.status(404)
+      throw new Error('User not found')
+    }
+  } catch (err) {
+      next(err)
+  }
+}
+
+
+// @desc  Update user profile
+// @route Put /api/users/profile
+// @access Private
+export const updateUserProfile = async(req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if (req.body.password) {
+        user.password = req.body.password
+      }
+
+      const updatedUser = await user.save()
+
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser._id)
+      })
+
     } else {
       res.status(404)
       throw new Error('User not found')
